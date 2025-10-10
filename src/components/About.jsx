69b33d9c5d4165be75e66446d3b29ba6be196115
Nastia Bzova <bzovanastia@gmail.com>
2025-10-10 12:48:27 +0200

    update about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,12 +9,14 @@ const About = () => {
             <iframe width="560" height="400" src="https://www.youtube.com/embed/549GzvKwPWw?si=GGtg1IA3Q1tGLfaX&amp;controls=0" title="Saint Catherine Academy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
           <div className="about-text">
-            <h2>About King's College</h2>
-              <p>Welcome to King's College, a distinguished educational institution dedicated to fostering academic excellence and developing strong character in its students. Located on the serene Old Northern Highway in Belize, our beautiful, tree-lined campus provides an ideal environment for learning and growth.</p>
-            <h3>Our History</h3>
-              <p>Established in 1967, King's College has proudly served the Belizean community for over five decades. Our foundation is built on a commitment to nurturing young men and women into ethical, knowledgeable, and globally aware citizens.</p>
-              <p>Our motto, "Future Leaders for the World," is the driving force behind everything we do. We strive to provide a balanced education that challenges the mind, encourages creativity, and instills the values necessary for leadership in the modern world.</p>
-          </div>
+            <h2>About Academy</h2>
+            <p>Welcome to St. Catherine Academy, a premier all-girls institution rooted in the Mercy tradition and dedicated to holistic education. Located on Hutson Street in the heart of Belize City, our historic campus provides a nurturing and challenging environment where young women are inspired to lead, serve, and excel.</p>
+            <p>SCA fosters intellectual curiosity, spiritual depth, and social responsibility, preparing students not just for higher education, but for meaningful lives as ethical leaders in their communities and the world.</p>
+
+            <h3>Our Heritage and Mission</h3>
+            <p>Founded in 1883 by the Sisters of Mercy, St. Catherine Academy holds a rich, enduring legacy as one of Belize's oldest and most respected educational institutions. Our mission is built upon the core values of the Sisters of Mercy, focusing on <strong>Compassion, Integrity, Excellence,</strong> and <strong>Service.</strong></p>
+            <p>Guided by the motto, "Virtue and Knowledge," we provide a rigorous academic program alongside a strong emphasis on character development and faith formation. We are committed to empowering every young woman to unlock her full potential and become a catalyst for positive change.</p>
+        </div>
         </div>
       </div>
     </section>
